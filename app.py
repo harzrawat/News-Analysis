6a77ad326b9b76b26d1ae51cf7145c01f8711bbc
Harsh Rawat <141559378+harzrawat@users.noreply.github.com>
2024-03-05 00:14:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask,render_template, url_for, request,redirect
-#import nltk
+import nltk
 from nltk import word_tokenize, sent_tokenize,pos_tag
 from nltk.corpus import stopwords
 import psycopg2
 import json
 
-import nltk
-
 def download_nltk_data():
     try:
         nltk.download('punkt')
+        nltk.download('stopwords')
     except LookupError:
-        pass  # Ignore if the model is already downloaded
+        pass  # Ignore if the data is already downloaded
 
 download_nltk_data()
 
-# Rest of your application logic
 
 
 app = Flask(__name__)
@@ -66,69 +64,74 @@ def index():
 
 @app.route("/data_submit", methods=['POST'])
 def data_submit():
-    text = request.form["inputText"]
-
-    def word_count(text):
-        c=0
-        for i in text:
-            if i==" ":
-                c+=1
-        if text[len(text)-1] != " ":
-            c+=1
-            return c
-        else:
-            return c
-            
-    
-    word_lst = word_tokenize(text)
-    word_count = word_count(text)
-
-    def sen_count(text):
-        c=0
-        for i in text:
-            if i in ['.','?']:
-                c+=1
-        return c
-    
-
-    sent_tokenize1 = sent_tokenize(text)
-    sen_count = sen_count(text)
-
-    stopwords_lst = stopwords.words('english')
-    def count_stop(word_lst):
-        c=0
-        for i in word_lst:
-            if i in stopwords_lst:
-                c+=1
-        return c
-
-    stop_count = count_stop(word_lst)
-
-    upos_tags = pos_tag(word_lst)
-    tag_count_dict = {}
-    
-    for word,tags in upos_tags:
-        if tags in tag_count_dict:
-            tag_count_dict[tags]+=1
-        else:
-            tag_count_dict[tags]=1
-    tag_count = len(tag_count_dict)
-    tag_count_dict = json.dumps(tag_count_dict)
-    
     try:
-        conn = psycopg2.connect(**db_params)
-        cur = conn.cursor()
+        text = request.form["inputText"]
 
-        cur.execute("""
-            insert into dat_table (Text,Word_Count,sent_count,stop_count,upos_tag_dict,upos_tag_count) values (%s,%s,%s,%s,%s,%s)""",(text,word_count,sen_count,stop_count,tag_count_dict,tag_count))
+        def word_count(text):
+            c=0
+            for i in text:
+                if i==" ":
+                    c+=1
+            if text[len(text)-1] != " ":
+                c+=1
+                return c
+            else:
+                return c
+                
+        
+        word_lst = word_tokenize(text)
+        word_count = word_count(text)
 
-        conn.commit()
-        cur.close()
-        conn.close()
+        def sen_count(text):
+            c=0
+            for i in text:
+                if i in ['.','?']:
+                    c+=1
+            return c
+        
 
-        return redirect(url_for('dashboard'))
-    except psycopg2.IntegrityError:
-        return "Error: User with the same name already exists."
+        sent_tokenize1 = sent_tokenize(text)
+        sen_count = sen_count(text)
+
+        stopwords_lst = stopwords.words('english')
+        def count_stop(word_lst):
+            c=0
+            for i in word_lst:
+                if i in stopwords_lst:
+                    c+=1
+            return c
+
+        stop_count = count_stop(word_lst)
+
+        upos_tags = pos_tag(word_lst)
+        tag_count_dict = {}
+        
+        for word,tags in upos_tags:
+            if tags in tag_count_dict:
+                tag_count_dict[tags]+=1
+            else:
+                tag_count_dict[tags]=1
+        tag_count = len(tag_count_dict)
+        tag_count_dict = json.dumps(tag_count_dict)
+        
+        try:
+            conn = psycopg2.connect(**db_params)
+            cur = conn.cursor()
+
+            cur.execute("""
+                insert into dat_table (Text,Word_Count,sent_count,stop_count,upos_tag_dict,upos_tag_count) values (%s,%s,%s,%s,%s,%s)""",(text,word_count,sen_count,stop_count,tag_count_dict,tag_count))
+
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            return redirect(url_for('dashboard'))
+        except psycopg2.IntegrityError:
+            return "Error: User with the same name already exists."
+    except Exception as e:
+        # Print the exception details for debugging
+        print(f"Exception occurred: {str(e)}")
+        return f"Error: {str(e)}"
 
 @app.route("/dashboard")
 def dashboard():
